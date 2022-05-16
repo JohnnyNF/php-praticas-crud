6cfb4fe92ac71747779b693e90560b8f339d0aa0
Jhonatan Feijo <90208145+JohnnyNF@users.noreply.github.com>
2022-05-16 20:50:34 -0300

    Inserindo o create

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Para começar a fazer um CRUD primeiro precisamos das configurações necessaria
 [Configurações](https://github.com/JohnnyNF/php-praticas-crud/blob/main/arquivos/config.php)
 com isso conseguimos ter o acesso as configurações do sistemas
 
+Logo após fazemos o [index](https://github.com/JohnnyNF/php-praticas-crud/blob/main/arquivos/index.php) inserimos dados primarios para desenvolver os CRUD's
--- arquivos/adicionar_action.php
+++ arquivos/adicionar_action.php
@@ -7,12 +7,23 @@ $email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
 
 
 // A verificação para ver se o nome e o email foi infomarmado e se estão corretos 
-// Caso contrario precisamos voltar para a minha pagina de adicionar e digitamos novamente os campos informados
 // Para inserir um novo usuario fazemos o seguinte
 if($name && $email){
 
+    // Vou chamar a variável $sql e transformar os valores que está na variável $name e $email
+    $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES(:name, :email)");
+    $sql->bindValue(':name', $name);
+     $sql->bindParam(':email', $email);
+    // Isso ira executar o banco para inserir 
+    $sql->execute();
+    
+    // Quando adicionar alguem eu volto para meu arquivo(index seria o arquivo).
+    // FAZENDO ISSO SERIA TUDO UMA INSERÇÃO DE DADOS  
+    header("Location: index.php");
+    exit;
 
 } 
+// Caso contrario precisamos voltar para a minha pagina de adicionar e digitamos novamente os campos informados
 else {
     header("Location: adicionar.php");
     exit;
