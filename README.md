6aead9a3145c804153f1c5b301afc798b6abe387
Jhonatan Feijo <jhonatanfeijo98@gmail.com>
2022-05-18 10:52:49 -0300

    inserindo create

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ com isso conseguimos ter o acesso as configurações do sistemas
 
 Logo após fazemos o [index](https://github.com/JohnnyNF/php-praticas-crud/blob/main/arquivos/index.php) inserimos dados primarios para desenvolver os CRUD's
 
-Após isso adicionammos a a verificação da query e as ações que vai ocorrer [Adiciona-Ação](https://github.com/JohnnyNF/php-praticas-crud/blob/main/arquivos/adicionar_action.php)
+Após isso adicionammos a a verificação da query e as ações que vai ocorrer [adiciona-ação](https://github.com/JohnnyNF/php-praticas-crud/blob/main/arquivos/adicionar_action.php)
+
+Verificamos se o email existe e se ele exister nós utilizamos uma consulta junto com uma verificação [verificação_com_rowCount()]()
--- arquivos/adicionar_action.php
+++ arquivos/adicionar_action.php
@@ -10,23 +10,33 @@ $email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
 // Para inserir um novo usuario fazemos o seguinte
 if($name && $email){
 
-    // Para fazer uma verificação se o email já existe 
+    // Fazer uma consulta no banco para ver se existe um email 
+    // Fazer uma verificação 
     $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql->bindValue(':email', $email);
+    
+    // Para fazer uma verificação se o email já existe no sistema utilizo o rowCount--->Vai me dizer a quantidade de registros
+    if ($sql->rowCount()===0){
     
     // Vou chamar a variável $sql e transformar os valores que está na variável $name e $email
     $sql = $pdo->prepare("INSERT INTO usuarios (nome, email) VALUES(:name, :email)");
     $sql->bindValue(':name', $name);
-    $sql->bindParam(':email', $email);
+    $sql->bindValue(':email', $email);
     // Isso ira executar o banco para inserir 
     $sql->execute();
-    
+
     // Quando adicionar alguem eu volto para meu arquivo(index seria o arquivo).
     // FAZENDO ISSO SERIA TUDO UMA INSERÇÃO DE DADOS  
     header("Location: index.php");
     exit;
 
-} 
-// Caso contrario precisamos voltar para a minha pagina de adicionar e digitamos novamente os campos informados
+}else{
+    // Caso contrario precisamos voltar para a minha pagina de adicionar e digitamos novamente os campos informados
+    header("Location: adicionar.php");
+    exit;
+}
+}
+
 else {
     header("Location: adicionar.php");
     exit;
